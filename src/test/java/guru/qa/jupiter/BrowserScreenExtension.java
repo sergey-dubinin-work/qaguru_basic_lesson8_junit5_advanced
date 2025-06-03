d72aff7f51aff8cab5777906e5d43f144fd3c46d
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-03 14:37:43 +0300

    add Screenshot on test fail using TestWatcher

--- src/test/java/guru/qa/jupiter/BrowserScreenExtension.java
+++ src/test/java/guru/qa/jupiter/BrowserScreenExtension.java
@@ -3,12 +3,13 @@ package guru.qa.jupiter;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
+import org.junit.jupiter.api.extension.TestWatcher;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
-public class BrowserScreenExtension implements TestExecutionExceptionHandler, AfterAllCallback {
+public class BrowserScreenExtension implements TestExecutionExceptionHandler, AfterAllCallback, TestWatcher {
 
     @Override
     public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
@@ -21,5 +22,11 @@ public class BrowserScreenExtension implements TestExecutionExceptionHandler, Af
     @Override
     public void afterAll(ExtensionContext context) throws Exception {
         getWebDriver().close();
+    }
+
+    @Override
+    public void testFailed(ExtensionContext context, Throwable cause) {
+        byte[] screenshot = ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
+        // add to Allure
     }
 }
