3d0daa7dd8a0382a35204048933b3f58dd5cf883
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-03 11:37:48 +0300

    argumentsConverter v1 (Bad practice)

--- src/test/java/guru/qa/ArgumentsExampleTest.java
+++ src/test/java/guru/qa/ArgumentsExampleTest.java
@@ -1,9 +1,18 @@
 package guru.qa;
 
+import com.google.gson.Gson;
+import guru.qa.domain.Teacher;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class ArgumentsExampleTest {
+
+    ClassLoader classLoader = getClass().getClassLoader();
 
     @ParameterizedTest
     @CsvSource(value = {
@@ -13,5 +22,25 @@ public class ArgumentsExampleTest {
     void paramTest(int first, boolean second) {
         System.out.println(String.valueOf(first) + second);
     }
-    
+
+    @ValueSource(strings = {
+            "files/dima.json",
+            "files/stas.json"
+    })
+    @ParameterizedTest
+    void apiParamTestV1(String teacher) throws IOException {
+        Teacher teacherObj;
+
+        Gson gson = new Gson();
+
+        try (InputStream inputStream = classLoader.getResourceAsStream(teacher)) {
+            String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            teacherObj = gson.fromJson(json, Teacher.class);
+        }
+
+        // В реальности будем отправлять объект Teacher в API
+        System.out.println(teacherObj.toString());
+
+    }
+
 }
